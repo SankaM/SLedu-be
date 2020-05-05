202257fb449d4a006d7392424e903b8dee4ef8eb
Priyantha Weerakoon <snkwrkn@gmail.com>
2020-05-06 04:06:13 +0800

    grade 6 sql

--- ajantha/src/main/resources/db/migration/V1.0.2__add_dummy_tbl_data_question.sql
+++ ajantha/src/main/resources/db/migration/V1.0.2__add_dummy_tbl_data_question.sql
@@ -10,6 +10,11 @@ insert into sledu.t_subject values(1, 1, 'Mathematics');
 insert into sledu.t_subject values(2, 2, 'ගණිතය');
 insert into sledu.t_subject values(3, 1, 'English');
 insert into sledu.t_subject values(4, 2, 'සිංහල');
+insert into sledu.t_subject values(5, 3, 'Mathematics');
+insert into sledu.t_subject values(6, 6, 'ගණිතය');
+insert into sledu.t_subject values(7, 3, 'English');
+insert into sledu.t_subject values(8, 6, 'සිංහල');
+
 
 insert into sledu.t_lesson values(1, 2, 'වර්ගඵලය', 'https://sl-edu.s3-ap-southeast-1.amazonaws.com/cover-images/_5area.jpg' );
 insert into sledu.t_lesson values(2, 2, 'පරිමිතිය', 'https://sl-edu.s3-ap-southeast-1.amazonaws.com/cover-images/_5parimithiya.png');
