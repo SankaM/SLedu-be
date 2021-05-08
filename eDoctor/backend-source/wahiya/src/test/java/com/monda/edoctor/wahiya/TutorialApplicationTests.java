658b2c725b76c5177e6e815d5f1c2495991fc313
sanka <priyanthaw520@gmail.com>
2021-05-09 07:50:19 +0800

    EDR-1: skeleton app

---
+++ eDoctor/backend-source/wahiya/src/test/java/com/monda/edoctor/wahiya/TutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.monda.edoctor.wahiya;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
